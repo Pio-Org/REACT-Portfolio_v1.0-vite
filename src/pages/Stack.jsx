477a4dd809891c1074d384178ae2d3c81cb7fd56
ggpio18 <piolo18aranza@gmail.com>
2025-03-03 23:40:21 +0800

    *=[starting Stack Section]

---
+++ src/pages/Stack.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SiFramer, SiFigma, SiReact, SiNodedotjs } from 'react-icons/si';
+import { FaLemon } from 'react-icons/fa';
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from 'react-intersection-observer';
+
+export const Stack = () => {
+
+    return(
+        <></>
+    );
+};
