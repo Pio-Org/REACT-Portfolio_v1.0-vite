1b4baea005a955c8e3ca74f2c5d61841e276dd25
ggpio18 <piolo18aranza@gmail.com>
2025-03-03 23:47:40 +0800

    *=[data| controls for stack section]

--- src/pages/Stack.jsx
+++ src/pages/Stack.jsx
@@ -1,10 +1,57 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SiFramer, SiFigma, SiReact, SiNodedotjs } from 'react-icons/si';
 import { FaLemon } from 'react-icons/fa';
-import { motion, useAnimation } from "framer-motion";
+import { color, motion, useAnimation } from "framer-motion";
 import { useInView } from 'react-intersection-observer';
 
+const stackItems= [
+    {
+        id: 1,
+        name: "Framer",
+        icon: <SiFramer size={100}/>,
+        color: "text-emerald-200",
+    },
+    {
+        id: 2,
+        name: "Figma",
+        icon: <SiFigma size={100}/>,
+        color: "text-emerald-200",
+    },
+    {
+        id: 3,
+        name: "React",
+        icon: <SiReact size={100}/>,
+        color: "text-emerald-200",
+    },
+    {
+        id: 4,
+        name: "Node.js",
+        icon: <SiNodedotjs size={100}/>,
+        color: "text-emerald-200",
+    },
+    {
+        id: 5,
+        name: "Lemon",
+        icon: <SFaLemon size={100}/>,
+        color: "text-emerald-200",
+    },
+];
+
 export const Stack = () => {
+
+    const controls = useAnimation();
+
+    const [ref, inView] = useInView({
+        threshold: 0.1,
+    });
+
+    useEffect(() => {
+        if (inView) {
+            controls.start("visible");
+        } else {
+            controls.start("hidden");
+        }
+    }, [controls. inView]);
 
     return(
         <></>
