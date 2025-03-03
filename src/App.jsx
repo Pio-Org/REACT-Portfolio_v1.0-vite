4364cd0d66fef76ac158a77885928b1747b25194
ggpio18 <piolo18aranza@gmail.com>
2025-03-03 23:01:20 +0800

    +=[About section]

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { About } from "./pages/About";
 import { Hero } from "./pages/Hero";
 
 export default function App() {
   return (
     <div>
      <Hero/>
-
+     <About/>
     </div>
   )
 }
