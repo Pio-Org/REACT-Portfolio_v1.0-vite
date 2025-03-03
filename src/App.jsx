d86cdf6c8c7abcd3f8db52529d1f1ebfb8689e0b
ggpio18 <piolo18aranza@gmail.com>
2025-03-04 00:05:53 +0800

    *=[Stacks ection error not rendering icons]

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { About } from "./pages/About";
 import { Hero } from "./pages/Hero";
 import { Portfolio } from "./pages/Portfolio";
+import { Stack } from "./pages/Stack";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
      <Hero/>
      <About/>
      <Portfolio/>
+     <Stack/>
     </div>
   )
 }
